de72b986c1970b40be7feb333681dc09b950e46d
CupcakeMagic <ralitza08@gmail.com>
2020-06-16 15:01:23 +0300

    Initial commit

---
+++ Assets/CyberPunk_Blonde/Sprites/Egypt_Gladiators/Scripts/GladiatorController.cs
@@ -0,0 +1,176 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class GladiatorController : MonoBehaviour {
+
+
+    SpriteRenderer Srend;
+    Animator anim;
+    Rigidbody2D rb;
+
+
+    //change these variables if you wish to test different speeds and jump heights
+    //[SerializeField]
+    //float moveForce = .1f;
+
+    [SerializeField]
+    float speed = 5.0f;
+
+
+    //this variable is used for the screen wrapping
+    float screenHalfWidthInWorldUnits;
+
+    bool screenWrap = false;
+
+
+
+    void Start()
+    {
+        Srend = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponentInChildren<Rigidbody2D>();
+
+        //these lines are used to calculate screen wrapping
+        //float halfPlayerWidth = transform.localScale.x / 2f;
+        //screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
+    // Update is called once per frame
+    void Update () {
+        //controller and sprite manipulation
+        #region
+        //controller and sprite manipulation
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetBool("WalkUp", true);
+
+            transform.Translate(Vector2.up * Time.fixedDeltaTime);
+         
+            anim.SetBool("Idle", false);
+        }else
+        {
+            anim.SetBool("WalkUp", false);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("WalkRight", true);
+            transform.Translate(Vector2.right * Time.fixedDeltaTime);
+            anim.SetBool("Idle", false);
+        }
+        else
+        {
+            anim.SetBool("WalkRight", false);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("WalkLeft", true);
+            transform.Translate(Vector2.left * Time.fixedDeltaTime);
+
+        }
+else        {
+            anim.SetBool("WalkLeft", false);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("WalkDown", true);
+
+            transform.Translate(Vector2.down * Time.fixedDeltaTime);
+
+        }
+        else
+        {
+            anim.SetBool("WalkDown", false);
+        }
+
+
+
+       
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            anim.SetBool("Idle", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetBool("Die", true);
+            anim.SetTrigger("Death");
+        }
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+       
+        }
+        #endregion // //controls and sprite manipulation
+        float moveInputHorisontal = Input.GetAxisRaw("Horizontal");
+        float moveInputVertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(moveInputHorisontal * speed, moveInputVertical * speed);
+        /*
+        if (moveInputHorisontal > 0)
+        {
+            anim.SetBool("WalkRight", true);
+        }
+        else
+        {
+            anim.SetBool("WalkRight", false);
+        }
+
+        if (moveInputHorisontal < 0)
+        {
+            anim.SetBool("WalkLeft", true);
+        }
+        else
+        {
+            anim.SetBool("WalkLeft", false);
+        }
+
+        if (moveInputVertical > 0)
+        {
+            anim.SetBool("WalkUp", true);
+        }
+        else
+        {
+            anim.SetBool("WalkUp", false);
+        }
+
+        if (moveInputVertical < 0)
+        {
+            anim.SetBool("WalkDown", true);
+        }
+        else
+        {
+            anim.SetBool("WalkDown", false);
+        }
+        */
+        //camera wrap
+        #region
+        //controls the camera wrap
+        /*
+        if (screenWrap)
+        {
+
+
+            if (transform.position.x < -screenHalfWidthInWorldUnits)
+            {
+                transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+            }
+
+            if (transform.position.x > screenHalfWidthInWorldUnits)
+            {
+                transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+            }
+        } */
+        #endregion//camera wrap 
+       
+    }
+}
+
---
+++ Assets/Ninja Files/Ninja Assets/Ninja.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInputHorisontal = Input.GetAxisRaw("Horizontal");
+        float moveInputVertical = Input.GetAxisRaw("Vertical");
+        Debug.Log("Хоризонтал: " + moveInputHorisontal + "    Вертикал: " + moveInputVertical);
+        rb.velocity = new Vector2(moveInputHorisontal * speed, moveInputVertical * speed);
+        if(moveInputHorisontal == 0 && moveInputVertical == 0)
+        {
+            anim.SetBool("IsMoving", false);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        if(moveInputHorisontal > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (moveInputHorisontal < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+    }
+}
